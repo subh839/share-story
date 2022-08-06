566fd56a85d2aab79a66eb432cd432584ba015cd
subh839 <79743346+subh839@users.noreply.github.com>
2022-08-06 17:15:01 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+// express middleware 
+//app initialize to express 
+app.use(bodyParser.json({ limit: '35mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '35mb', extended: true }))
+// bodyparser to populate senders req
+app.use(cors());
+// cors must be above of postRoutes
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://shubha:shubha1234@cluster0.ezibl.mongodb.net/?retryWrites=true&w=majority';
+// string
+const PORT = process.env.PORT|| 5000;
+// connect database
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//return a promise
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  // , callback function 
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
---
+++ server/models/postMessage.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    //convert img into string 
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    //obj:
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+})
+
+var PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
