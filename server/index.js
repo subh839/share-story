5ca04f086e4f9152ae0e1bb30469167b52060a50
Subhasish Halder <79743346+subh839@users.noreply.github.com>
2022-08-07 23:06:56 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
-
+//require('dotenv').config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
-
+//const dotenv=require('dotenv')
+//dotenv.config();
+'https://github.com/subh839/share-story.git'
 const app = express();
 // express middleware 
 //app initialize to express 
+app.use(cors());
+// cors must be above of postRoutes
 app.use(bodyParser.json({ limit: '35mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '35mb', extended: true }))
 // bodyparser to populate senders req
-app.use(cors());
-// cors must be above of postRoutes
+
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://shubha:shubha1234@cluster0.ezibl.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL ='mongodb+srv://shubha:shubha1234@cluster0.ezibl.mongodb.net/?retryWrites=true&w=majority';
 // string
 const PORT = process.env.PORT|| 5000;
 // connect database
