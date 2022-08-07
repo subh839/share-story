1c99c1fdf378d56ceadee19205466ad122a3ad5e
Subhasish Halder <79743346+subh839@users.noreply.github.com>
2022-08-07 23:06:08 +0530

    bugfixed

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost,dislikePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
@@ -10,5 +10,5 @@ router.get('/:id', getPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
 router.patch('/:id/likePost', likePost);
-
+router.patch('/:id/dislikePost', dislikePost);
 export default router;
